e0608ed9ca77b9ef3fa692662f5e7aaeda3ada37
Ulil Albab <aoohan@Ulils-MacBook-Pro.local>
2022-03-19 18:53:52 +0700

    initial commit and add react environment variabels

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const spotifyClientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
